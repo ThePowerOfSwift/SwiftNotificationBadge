772be9067252c4ea4a1725e8f5e3d6fba5b8e330
Manish Rathi <manishrathi19902013@gmail.com>
2019-02-10 00:13:40 +0530

    Initial commit of SwiftNotificationBadge project.

---
+++ Source/SwiftNotificationBadge/SwiftNotificationBadge/SwiftNotificationBadge.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNotificationBadge.h
+//  SwiftNotificationBadge
+//
+//  Created by Manish Rathi on 10/02/19.
+//  Copyright © 2019 Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNotificationBadge.
+FOUNDATION_EXPORT double SwiftNotificationBadgeVersionNumber;
+
+//! Project version string for SwiftNotificationBadge.
+FOUNDATION_EXPORT const unsigned char SwiftNotificationBadgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNotificationBadge/PublicHeader.h>
+
+
